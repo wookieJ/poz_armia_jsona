c353d6c81e980fb2b657745203ff4cfa549e46a6
Franek <Franek>
2018-04-20 22:34:29 +0200

    Add allegroAPI support

--- src/main/java/com/ArmiaJsona/emojiSearch/EmojiSearchApplication.java
+++ src/main/java/com/ArmiaJsona/emojiSearch/EmojiSearchApplication.java
@@ -1,5 +1,6 @@
 package com.ArmiaJsona.emojiSearch;
 
+import com.ArmiaJsona.emojiSearch.allegro.AllegroClient;
 import com.ArmiaJsona.emojiSearch.emoji.EmojiClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -12,6 +13,9 @@ public class EmojiSearchApplication implements CommandLineRunner {
 	@Autowired
 	private EmojiClient emojipediaClient;
 
+	@Autowired
+	private AllegroClient allegroClient;
+
 	public static void main(String[] args) {
 		SpringApplication.run(EmojiSearchApplication.class, args);
 	}
@@ -19,5 +23,6 @@ public class EmojiSearchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.print(emojipediaClient.getEmojiNameByUnicode("\uD83C\uDF47"));
+		System.out.println(allegroClient.getOffersByPhrase("pilka nike"));
 	}
 }
---
+++ src/main/java/com/ArmiaJsona/emojiSearch/allegro/AllegroClient.java
@@ -0,0 +1,37 @@
+package com.ArmiaJsona.emojiSearch.allegro;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@Service
+public class AllegroClient implements AllegroClientInterface {
+
+    @Value("${AllegroAPI.URL}")
+    private String allegroApiUrl;
+    private String API_KEY = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJhbGxlZ3JvX2FwaSJdLCJleHAiOjE1MjQyOTEyMzMsImp0aSI6IjMyYmQzOTNjLWRhN2ItNDhiOS1hYzlkLTFjNjljZDg1OTBkNCIsImNsaWVudF9pZCI6ImE0MWY1YjJhLThlODctNGI4Yi1iNmZlLTc0Y2M3NjM3MjBkNyJ9.bVHSZCQ-_4DA_bR7UiIaXFXbZJKflePKkuUMYn0wSNcrrBGKZBaZDddxuyU5PF4nKKkuiGg8RBFY69tC4EjeNx2TExug6qwVQiAD5pfL-czDMwI9L1A5N7lc8-LIWbpffmTEXomyd6ezGWPehOpqRUYTGu3AImJJvptDgiXUmeBPgCs6zlm3gV856C--VuSO7AFtIoG6DbLtq_IOEPQt6Zdn-FFMxA-zu2BjeMDKUjuW_rFFEm0i5NGh72dYNPRmOObKa59Fvzbhl2uPwsBUPRgm5rFI8x8WeWdDzwY5feOPZuZJkYXQ6oeiLViOhnqIa6F763Zutk-hVeOr-PbN-Q";
+    private RestTemplate allegroRestTemplate;
+
+    public AllegroClient(RestTemplate allegroRestTemplate) {
+        this.allegroRestTemplate = allegroRestTemplate;
+    }
+
+    @Override
+    public String getOffersByPhrase(String phrase) {
+       return allegroRestTemplate.exchange(allegroApiUrl + "offers/?phrase=" + phrase, HttpMethod.GET, getUserAgent(), String.class ).getBody();
+    }
+
+    private HttpEntity getUserAgent() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.put(HttpHeaders.USER_AGENT, Arrays.asList("Mozilla/5.0 (Client-Id 656cbe47-b17d-46c2-bae1-3222c8777d5b) Gecko/20100101 Firefox/59.0"));
+        headers.put(HttpHeaders.AUTHORIZATION, Arrays.asList("Bearer "+ API_KEY));
+        headers.put(HttpHeaders.ACCEPT, Arrays.asList("application/vnd.allegro.public.v1+json"));
+        HttpEntity entity = new HttpEntity(headers);
+        return entity;
+    }
+}
