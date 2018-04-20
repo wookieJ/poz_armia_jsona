5eccb1c589ca8032c706a2064154ccca503876ed
Kamil Pabin <pabin.kamil@gmail.com>
2018-04-20 19:20:13 +0200

    Init commit

---
+++ src/main/java/com/ArmiaJsona/emojiSearch/EmojiSearchApplication.java
@@ -0,0 +1,12 @@
+package com.ArmiaJsona.emojiSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmojiSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmojiSearchApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ArmiaJsona/emojiSearch/EmojiSearchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ArmiaJsona.emojiSearch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmojiSearchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
