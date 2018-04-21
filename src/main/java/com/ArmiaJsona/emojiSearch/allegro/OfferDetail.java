a79c48ee6dead8bef5d9d1e0dbbc02ae20b80599
wookieJ <lukaszjedryczka.biuro@gmail.com>
2018-04-21 09:27:33 +0200

    Quantities, MainImage added in offers/{id} endpoint

--- src/main/java/com/ArmiaJsona/emojiSearch/allegro/OfferDetail.java
+++ src/main/java/com/ArmiaJsona/emojiSearch/allegro/OfferDetail.java
@@ -1,10 +1,14 @@
 package com.ArmiaJsona.emojiSearch.allegro;
 
 import com.ArmiaJsona.emojiSearch.model.Description;
+import com.ArmiaJsona.emojiSearch.model.MainImage;
+import com.ArmiaJsona.emojiSearch.model.Quantities;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OfferDetail {
     @JsonProperty("id")
     private String offerId;
@@ -14,29 +18,36 @@ public class OfferDetail {
     @JsonProperty("description")
     private Description offerDescription;
     private String description;
-    private long views;
-    private long endingTime;
+    private int views;
+    private int endingTime;
+    private Quantities quantities;
+    private MainImage mainImage;
 
     public String getDescription() {
         return offerDescription.getPlainText();
     }
 
-    public long getViews() {
+    public int getViews() {
         return views;
     }
 
-    public long getEndingTime() {
+    public int getEndingTime() {
         return endingTime;
     }
 
-    public OfferDetail(){
+    public Quantities getQuantities() {
+        return quantities;
+    }
+
+    public MainImage getMainImage() {
+        return mainImage;
+    }
+
+    public OfferDetail() {
     }
 
     @Override
     public String toString() {
-        return "OfferDetail{" +
-                "id='" + offerId + '\'' +
-                ", name='" + offerName + '\'' +
-                '}';
+        return "OfferDetail{" + "id='" + offerId + '\'' + ", name='" + offerName + '\'' + '}';
     }
 }
--- src/main/java/com/ArmiaJsona/emojiSearch/model/Url.java
+++ src/main/java/com/ArmiaJsona/emojiSearch/model/Url.java
@@ -1,5 +1,10 @@
 package com.ArmiaJsona.emojiSearch.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 class Url {
     private String url;
 
