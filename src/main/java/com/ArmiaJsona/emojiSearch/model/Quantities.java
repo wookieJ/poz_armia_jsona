704b8c8d3e82220dd5da1176544d531013419370
wookieJ <lukaszjedryczka.biuro@gmail.com>
2018-04-21 09:28:31 +0200

    Quantities, MainImage added in offers/{id} endpoint

---
+++ src/main/java/com/ArmiaJsona/emojiSearch/model/Quantities.java
@@ -0,0 +1,39 @@
+package com.ArmiaJsona.emojiSearch.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Quantities {
+    private int sold;
+    private int available;
+    private int type;
+
+    public int getSold() {
+        return sold;
+    }
+
+    public void setSold(int sold) {
+        this.sold = sold;
+    }
+
+    public int getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(int available) {
+        this.available = available;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public Quantities(){
+    }
+}
