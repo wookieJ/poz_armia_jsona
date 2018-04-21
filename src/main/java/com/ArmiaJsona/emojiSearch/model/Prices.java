f02e8b7c348a60c98d9c72929f42448d9ae622b4
wookieJ <lukaszjedryczka.biuro@gmail.com>
2018-04-21 03:16:20 +0200

    Prices in search endpoint repaired

--- src/main/java/com/ArmiaJsona/emojiSearch/model/Prices.java
+++ src/main/java/com/ArmiaJsona/emojiSearch/model/Prices.java
@@ -2,32 +2,23 @@ package com.ArmiaJsona.emojiSearch.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Prices {
     private Price buyNow;
-    private Price withDelivery;
     private Price current;
-
-    public Price getBuyNow() {
-        return buyNow;
-    }
 
     public void setBuyNow(Price buyNow) {
         this.buyNow = buyNow;
     }
 
-    public Price getWithDelivery() {
-        return withDelivery;
-    }
-
-    public void setWithDelivery(Price withDelivery) {
-        this.withDelivery = withDelivery;
-    }
-
-    public Price getCurrent() {
-        return current;
+    @JsonProperty("price")
+    public Price getPrice() {
+        if(current != null)
+            return current;
+        return buyNow;
     }
 
     public void setCurrent(Price current) {
